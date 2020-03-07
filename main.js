13fada05aed5d41aa85e8fd1b410ca53f471f943
Tyler French <tylerfrench89@gmail.com>
2020-03-07 09:13:48 -0800

    last commit

--- main.js
+++ main.js
@@ -5,15 +5,21 @@ const gridItemList = results.map((item) => {
         title: item.title,
         image: item.Images[0].url_fullxfull,
         shopName: item.Shop.shop_name,
+        price: item.price,
     }
 });
 
 const gridItems = gridItemList.map((item) => {
     return (
       `<div class="product" title="${item.title}">
+        <div class="circle">
+            <img class="heart" src="assets/heart.png" />
+        </div>
          <div class="product-img" style="background-image: url(${item.image});"></div>
          <h3 class="product-header">${item.title}</h3>
          <span>${item.shopName}</span>
+         <br>
+         <span>$${item.price}</span>
        </div>`
     )
 }).join('')
